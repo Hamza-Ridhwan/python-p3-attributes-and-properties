172e8e26f025577ec4e073383418a2bd6fdc81c4
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-12-08 14:12:33 +0300

    Completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,38 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Tiv", breed='Pointer'):
+        self.name = name
+        self.breed = breed
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+# d= Dog()
+# print(d.breed)
+# d.breed = 'bh'
+# print(d.breed)
+# d = Dog('tiv')
+# d.name = 'Rex'
+# print(d.name)
+# d.name = 'lkjsadlkj;lsakdjlksajdlkjaskdjlksajdlkjsadlkj'
+# print(getattr(d, 'name'))
+
